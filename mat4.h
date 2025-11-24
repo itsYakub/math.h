08cc835caa4243fe81feb0b0b25c314434decb3a
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-11-24 19:50:03 +0100

    mat4.h: structure variable change
    mat4 elements were changed from mat4::m0 - mat4::m15 to mat4::m00 - mat4::m33. This will allow more readability of the elements of the matrix and where each element lands as a representation of rows and columns. Implementation for the future mat2 and mat3 will follow the same convention. Also, 'float ptr[4][4]' was removed and will probably be replaced with a macro to the pointer to the first element of the matrix (helpful for working with OpenGL and, for example, passing data types into shaders).

--- mat4.h
+++ mat4.h
@@ -31,48 +31,40 @@ extern "C" {
 typedef union u_mat4 mat4;
 union u_mat4 {
     struct {
-        float m0,  m1,  m2,  m3,
-              m4,  m5,  m6,  m7,  
-              m8,  m9,  m10, m11,  
-              m12, m13, m14, m15;
+        float m00, m01, m02, m03,
+              m10, m11, m12, m13,
+              m20, m21, m22, m23,
+              m30, m31, m32, m33;
     };
 
     struct {
-        vec4 v0, v1, v2, v3;
+        vec4 m0, m1, m2, m3;
     };
-
-    float ptr[4][4];
 };
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 static inline mat4 mat4_zero(void) {
-    mat4 mat;
-    
-    for (size_t i = 0; i < sizeof(mat4); i++) {
-        ((unsigned char *) mat.ptr)[i] = 0;
-    }
+    mat4 mat = { .m00 = 0.0, .m01 = 0.0, .m02 = 0.0, .m03 = 0.0,
+                 .m10 = 0.0, .m11 = 0.0, .m12 = 0.0, .m13 = 0.0,
+                 .m20 = 0.0, .m21 = 0.0, .m22 = 0.0, .m23 = 0.0,
+                 .m30 = 0.0, .m31 = 0.0, .m32 = 0.0, .m33 = 0.0  };
     return (mat);
 }
 
 static inline mat4 mat4_identity(void) {
-    mat4 mat = mat4_zero();
-    mat.m0   = 1.0;
-    mat.m5   = 1.0;
-    mat.m10  = 1.0;
-    mat.m15  = 1.0;
+    mat4 mat = { .m00 = 1.0, .m01 = 0.0, .m02 = 0.0, .m03 = 0.0,
+                 .m10 = 0.0, .m11 = 1.0, .m12 = 0.0, .m13 = 0.0,
+                 .m20 = 0.0, .m21 = 0.0, .m22 = 1.0, .m23 = 0.0,
+                 .m30 = 0.0, .m31 = 0.0, .m32 = 0.0, .m33 = 1.0  };
     return (mat);
-}
-
-static inline mat4 mat4_init(void) {
-    return (mat4_zero());
 }
 
 static inline mat4 mat4_copy(const mat4 m0) {
     mat4 mat;
     
     for (size_t i = 0; i < sizeof(mat4); i++) {
-        ((unsigned char *) mat.ptr)[i] = ((unsigned char *) m0.ptr)[i];
+        ((unsigned char *) &mat.m00)[i] = ((unsigned char *) &m0.m00)[i];
     }
     return (mat);
 }
@@ -82,10 +74,10 @@ static inline mat4 mat4_copy(const mat4 m0) {
 static inline mat4 mat4_add(const mat4 m0, const mat4 m1) {
     mat4 mat;
 
-    mat.v0 = vec4_add(m0.v0, m1.v0);
-    mat.v1 = vec4_add(m0.v1, m1.v1);
-    mat.v2 = vec4_add(m0.v2, m1.v2);
-    mat.v3 = vec4_add(m0.v3, m1.v3);
+    mat.m0 = vec4_add(m0.m0, m1.m0);
+    mat.m1 = vec4_add(m0.m1, m1.m1);
+    mat.m2 = vec4_add(m0.m2, m1.m2);
+    mat.m3 = vec4_add(m0.m3, m1.m3);
     return (mat);
 }
 
@@ -108,10 +100,10 @@ const static inline mat4& operator += (mat4 &v0, const mat4 &v1) {
 static inline mat4 mat4_sub(const mat4 m0, const mat4 m1) {
     mat4 mat;
 
-    mat.v0 = vec4_sub(m0.v0, m1.v0);
-    mat.v1 = vec4_sub(m0.v1, m1.v1);
-    mat.v2 = vec4_sub(m0.v2, m1.v2);
-    mat.v3 = vec4_sub(m0.v3, m1.v3);
+    mat.m0 = vec4_sub(m0.m0, m1.m0);
+    mat.m1 = vec4_sub(m0.m1, m1.m1);
+    mat.m2 = vec4_sub(m0.m2, m1.m2);
+    mat.m3 = vec4_sub(m0.m3, m1.m3);
     return (mat);
 }
 
@@ -134,35 +126,35 @@ const static inline mat4& operator -= (mat4 &v0, const mat4 &v1) {
 static inline mat4 mat4_mul(const mat4 m0, const mat4 m1) {
     mat4 mat;
 
-    mat.m0  = m0.m0  * m1.m0 + m0.m1  * m1.m4 + m0.m2  * m1.m8  + m0.m3  * m1.m12;
-    mat.m1  = m0.m0  * m1.m1 + m0.m1  * m1.m5 + m0.m2  * m1.m9  + m0.m3  * m1.m13;
-    mat.m2  = m0.m0  * m1.m2 + m0.m1  * m1.m6 + m0.m2  * m1.m10 + m0.m3  * m1.m14;
-    mat.m3  = m0.m0  * m1.m3 + m0.m1  * m1.m7 + m0.m2  * m1.m11 + m0.m3  * m1.m15;
+    mat.m00 = m0.m00 * m1.m00 + m0.m01 * m1.m10 + m0.m02 * m1.m20 + m0.m03 * m1.m30;
+    mat.m01 = m0.m00 * m1.m01 + m0.m01 * m1.m11 + m0.m02 * m1.m21 + m0.m03 * m1.m31;
+    mat.m02 = m0.m00 * m1.m02 + m0.m01 * m1.m12 + m0.m02 * m1.m22 + m0.m03 * m1.m32;
+    mat.m03 = m0.m00 * m1.m03 + m0.m01 * m1.m13 + m0.m02 * m1.m23 + m0.m03 * m1.m33;
     
-    mat.m4  = m0.m4  * m1.m0 + m0.m5  * m1.m4 + m0.m6  * m1.m8  + m0.m7  * m1.m12;
-    mat.m5  = m0.m4  * m1.m1 + m0.m5  * m1.m5 + m0.m6  * m1.m9  + m0.m7  * m1.m13;
-    mat.m6  = m0.m4  * m1.m2 + m0.m5  * m1.m6 + m0.m6  * m1.m10 + m0.m7  * m1.m14;
-    mat.m7  = m0.m4  * m1.m3 + m0.m5  * m1.m7 + m0.m6  * m1.m11 + m0.m7  * m1.m15;
-
-    mat.m8  = m0.m8  * m1.m0 + m0.m9  * m1.m4 + m0.m10 * m1.m8  + m0.m11 * m1.m12;
-    mat.m9  = m0.m8  * m1.m1 + m0.m9  * m1.m5 + m0.m10 * m1.m9  + m0.m11 * m1.m13;
-    mat.m10 = m0.m8  * m1.m2 + m0.m9  * m1.m6 + m0.m10 * m1.m10 + m0.m11 * m1.m14;
-    mat.m11 = m0.m8  * m1.m3 + m0.m9  * m1.m7 + m0.m10 * m1.m11 + m0.m11 * m1.m15;
+    mat.m10 = m0.m10 * m1.m10 + m0.m11 * m1.m10 + m0.m12 * m1.m20 + m0.m13 * m1.m30;
+    mat.m11 = m0.m10 * m1.m11 + m0.m11 * m1.m11 + m0.m12 * m1.m21 + m0.m13 * m1.m31;
+    mat.m12 = m0.m10 * m1.m12 + m0.m11 * m1.m12 + m0.m12 * m1.m22 + m0.m13 * m1.m32;
+    mat.m13 = m0.m10 * m1.m13 + m0.m11 * m1.m13 + m0.m12 * m1.m23 + m0.m13 * m1.m33;
     
-    mat.m12 = m0.m12 * m1.m0 + m0.m13 * m1.m4 + m0.m14 * m1.m8  + m0.m15 * m1.m12;
-    mat.m13 = m0.m12 * m1.m1 + m0.m13 * m1.m5 + m0.m14 * m1.m9  + m0.m15 * m1.m13;
-    mat.m14 = m0.m12 * m1.m2 + m0.m13 * m1.m6 + m0.m14 * m1.m10 + m0.m15 * m1.m14;
-    mat.m15 = m0.m12 * m1.m3 + m0.m13 * m1.m7 + m0.m14 * m1.m11 + m0.m15 * m1.m15;
+    mat.m20 = m0.m20 * m1.m20 + m0.m21 * m1.m10 + m0.m22 * m1.m20 + m0.m23 * m1.m30;
+    mat.m21 = m0.m20 * m1.m21 + m0.m21 * m1.m11 + m0.m22 * m1.m21 + m0.m23 * m1.m31;
+    mat.m22 = m0.m20 * m1.m22 + m0.m21 * m1.m12 + m0.m22 * m1.m22 + m0.m23 * m1.m32;
+    mat.m23 = m0.m20 * m1.m23 + m0.m21 * m1.m13 + m0.m22 * m1.m23 + m0.m23 * m1.m33;
+    
+    mat.m30 = m0.m30 * m1.m30 + m0.m31 * m1.m10 + m0.m32 * m1.m20 + m0.m33 * m1.m30;
+    mat.m31 = m0.m30 * m1.m31 + m0.m31 * m1.m11 + m0.m32 * m1.m21 + m0.m33 * m1.m31;
+    mat.m32 = m0.m30 * m1.m32 + m0.m31 * m1.m12 + m0.m32 * m1.m22 + m0.m33 * m1.m32;
+    mat.m33 = m0.m30 * m1.m33 + m0.m31 * m1.m13 + m0.m32 * m1.m23 + m0.m33 * m1.m33;
     return (mat);
 }
 
 static inline mat4 mat4_mulf(const mat4 m0, const float f) {
     mat4 mat;
 
-    mat.v0 = vec4_mulf(m0.v0, f);
-    mat.v1 = vec4_mulf(m0.v1, f);
-    mat.v2 = vec4_mulf(m0.v2, f);
-    mat.v3 = vec4_mulf(m0.v3, f);
+    mat.m0 = vec4_mulf(m0.m0, f);
+    mat.m1 = vec4_mulf(m0.m1, f);
+    mat.m2 = vec4_mulf(m0.m2, f);
+    mat.m3 = vec4_mulf(m0.m3, f);
     return (mat);
 }
 
@@ -184,25 +176,25 @@ const static inline mat4& operator *= (mat4 &v0, const mat4 &v1) {
 
 static inline mat4 mat4_ortho(const float left, const float right, const float top, const float down, const float near, const float far) {
     mat4 mat = mat4_zero();
-    mat.m0   = 2.0 / (right - left);
-    mat.m5   = 2.0 / (top   - down);
-    mat.m10  = 2.0 / (far   - near);
-    mat.m12  = -(left + right) / (right - left);
-    mat.m13  = -(top + down) / (top - down);
-    mat.m14  = -(far + near) / (far - near);
-    mat.m15  = 1.0;
+    mat.m00  = 2.0 / (right - left);
+    mat.m11  = 2.0 / (top   - down);
+    mat.m22  = 2.0 / (far   - near);
+    mat.m30  = -(left + right) / (right - left);
+    mat.m31  = -(top + down) / (top - down);
+    mat.m32  = -(far + near) / (far - near);
+    mat.m33  = 1.0;
     return (mat);
 }
 
 static inline mat4 mat4_frust(const float left, const float right, const float top, const float down, const float near, const float far) {
     mat4 mat = mat4_zero();
-    mat.m0   = (near * 2.0) / (right - left);
-    mat.m5   = (near * 2.0) / (top   - down);
-    mat.m8   = (left + right) / (right - left);
-    mat.m9   = (top + down) / (top - down);
-    mat.m10  = -(far + near) / (far - near);
-    mat.m11  = -1.0;
-    mat.m14  = -(far * near * 2.0) / (far - near);
+    mat.m00  = (near * 2.0) / (right - left);
+    mat.m11  = (near * 2.0) / (top   - down);
+    mat.m20  = (left + right) / (right - left);
+    mat.m21  = (top + down) / (top - down);
+    mat.m22  = -(far + near) / (far - near);
+    mat.m23  = -1.0;
+    mat.m32  = -(far * near * 2.0) / (far - near);
     return (mat);
 }
 
@@ -215,7 +207,7 @@ static inline mat4 mat4_persp(const float fieldOfView, const float aspect, const
 /* ---------------------------------------------------------------------------------------------------- */
 
 static inline bool mat4_equals(const mat4 m0, const mat4 m1) {
-    return (vec4_equals(m0.v0, m1.v0) && vec4_equals(m0.v1, m1.v1) && vec4_equals(m0.v2, m1.v2) && vec4_equals(m0.v3, m1.v3));
+    return (vec4_equals(m0.m0, m1.m0) && vec4_equals(m0.m1, m1.m1) && vec4_equals(m0.m2, m1.m2) && vec4_equals(m0.m3, m1.m3));
 }
 
 # if defined (__cplusplus)
@@ -238,10 +230,10 @@ static inline const char *mat4_string(const mat4 m0) {
     static char buf[1024];
 
     if (!memset(buf, 0, sizeof(buf))) { return (0); }
-    strcat(buf, "[ "), strcat(buf, vec4_string(m0.v0)); strcat(buf, " ]\n");
-    strcat(buf, "[ "), strcat(buf, vec4_string(m0.v1)); strcat(buf, " ]\n");
-    strcat(buf, "[ "), strcat(buf, vec4_string(m0.v2)); strcat(buf, " ]\n");
-    strcat(buf, "[ "), strcat(buf, vec4_string(m0.v3)); strcat(buf, " ]\n");
+    strcat(buf, "[ "), strcat(buf, vec4_string(m0.m0)); strcat(buf, " ]\n");
+    strcat(buf, "[ "), strcat(buf, vec4_string(m0.m1)); strcat(buf, " ]\n");
+    strcat(buf, "[ "), strcat(buf, vec4_string(m0.m2)); strcat(buf, " ]\n");
+    strcat(buf, "[ "), strcat(buf, vec4_string(m0.m3)); strcat(buf, " ]\n");
     return (buf);
 }
 
