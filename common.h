d0e13860f6670d3bdf2c335a71ac47ce7797b0e3
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-11-27 06:35:41 +0100

    common.h: macro guards
    common.h's funcions now uses macro guards to ensure that if the label is already defined we don't want to define the desired label of the function (i.e. _pow() -> pow()).

--- common.h
+++ common.h
@@ -61,13 +61,27 @@ static inline size_t _fact(size_t v0) {
     return (value);
 }
 
-# define abs(x)     _abs(x)
-# define trunc(x)   _trunc(x)
-# define mod(x)     _mod(x)
-# define ceil(x)    _ceil(x)
-# define floor(x)   _floor(x)
-# define round(x)   _round(x)
-# define fact(x)    _fact(x)
+# if !defined (abs)
+#  define abs(x)     _abs(x)
+# endif /* abs */
+# if !defined (trunc)
+#  define trunc(x)   _trunc(x)
+# endif /* trunc */
+# if !defined (mod)
+#  define mod(x)     _mod(x)
+# endif /* mod */
+# if !defined (ceil)
+#  define ceil(x)    _ceil(x)
+# endif /* ceil */
+# if !defined (floor)
+#  define floor(x)   _floor(x)
+# endif /* floor */
+# if !defined (round)
+#  define round(x)   _round(x)
+# endif /* round */
+# if !defined (fact)
+#  define fact(x)    _fact(x)
+# endif /* fact */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -81,8 +95,12 @@ static inline float _max(const float f0, const float f1) {
                  { return (f1); }
 }
 
-# define min(x, y)  _min(x, y)
-# define max(x, y)  _max(x, y)
+# if !defined (min)
+#  define min(x, y)  _min(x, y)
+# endif /* min */
+# if !defined (max)
+#  define max(x, y)  _max(x, y)
+# endif /* max */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -98,7 +116,9 @@ static inline float _exp(const float f0) {
     return (result);
 }
 
-# define exp(x)     _exp(x)
+# if !defined (exp)
+#  define exp(x)     _exp(x)
+# endif /* exp */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -116,28 +136,19 @@ static inline float _logn(const float f0) {
 static inline float _log(const float f0, const float f1) { return (_logn(f0) / _logn(f1)); }
 
 
-# define logn(x)    _logn(x)
-# define log(x)     _log(x)
+# if !defined (logn)
+#  define logn(x)    _logn(x)
+# endif /* logn */
+# if !defined (log)
+#  define log(x, y)  _log(x, y)
+# endif /* log */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 static inline float _pow(const float f0, const float f1) {
-    if (f1 < 0.0)  { return (1.0 / _pow(f0, abs(f1))); }
+    if (f1 < 0.0)  { return (1.0 / _pow(f0, _abs(f1))); }
     if (f1 == 0.0) { return (1.0); }
 
-    /* Perform regular iterative operation on the integer exponent...
-     * */
-    if (f1 - (int) f1 == 0.0) {
-        float result = 1.0;
-        for (size_t i = 0; i < (size_t) f1; i++) {
-            result *= f0;
-        }
-
-        return (result);
-    }
-
-    /* Perform exponential and logarithmic operation on floating-point exponent...
-     * */
     return (_exp(f1 * _logn(f0)));
 }
 
@@ -159,16 +170,24 @@ static inline float _sqrt(const float f0) {
     return (middle);
 }
 
-# define pow(x, y)  _pow(x, y)
-# define sqrt(x)    _sqrt(x)
+# if !defined (pow)
+#  define pow(x, y)  _pow(x, y)
+# endif /* logn */
+# if !defined (sqrt)
+#  define sqrt(x)    _sqrt(x)
+# endif /* logn */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 static inline float _degToRad(const float f0) { return (f0 * (PI / 180.0)); }
 static inline float _radToDeg(const float f0) { return (f0 * (180.0 / PI)); }
 
-# define degToRad(x)    _degToRad(x)
-# define radToDeg(x)    _radToDeg(x)
+# if !defined (degToRad)
+#  define degToRad(x)    _degToRad(x)
+# endif /* degToRad */
+# if !defined (radToDeg)
+#  define radToDeg(x)    _radToDeg(x)
+# endif /* radToDeg */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -194,10 +213,18 @@ static inline float _tan(float f0) { return (_sin(f0) / _cos(f0)); }
 
 static inline float _cot(float f0) { return (1.0 / _tan(f0)); }
 
-# define sin(x)     _sin(x)
-# define cos(x)     _cos(x)
-# define tan(x)     _tan(x)
-# define cot(x)     _cot(x)
+# if !defined (sin)
+#  define sin(x)     _sin(x)
+# endif /* sin */
+# if !defined (cos)
+#  define cos(x)     _cos(x)
+# endif /* cos */
+# if !defined (tan)
+#  define tan(x)     _tan(x)
+# endif /* tan */
+# if !defined (cot)
+#  define cot(x)     _cot(x)
+# endif /* cot */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -237,11 +264,21 @@ static inline float _atan2(float f0, float f1) {
 
 static inline float _acot(float f0) { return (PI / 2.0 - _atan(f0)); }
 
-# define asin(x)     _asin(x)
-# define acos(x)     _acos(x)
-# define atan(x)     _atan(x)
-# define acot(x)     _acot(x)
-# define atan2(x, y) _atan2(x, y)
+# if !defined (asin)
+#  define asin(x)     _asin(x)
+# endif /* asin */
+# if !defined (acos)
+#  define acos(x)     _acos(x)
+# endif /* acos */
+# if !defined (atan)
+#  define atan(x)     _atan(x)
+# endif /* atan */
+# if !defined (atan2)
+#  define atan2(x, y) _atan2(x, y)
+# endif /* atan2 */
+# if !defined (acot)
+#  define acot(x)     _acot(x)
+# endif /* acot */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -253,8 +290,12 @@ static inline float _clamp(const float f0, const float min, const float max) {
 
 static inline float clamp01(const float f0) { return (_clamp(f0, 0.0, 1.0)); }
 
-# define clamp(x, y, z) _clamp(x, y, z)
-# define clamp01(x)     _clamp(x)
+# if !defined (clamp)
+#  define clamp(x, y, z) _clamp(x, y, z)
+# endif /* clamp */
+# if !defined (clamp01)
+#  define clamp01(x)     _clamp(x)
+# endif /* clamp01 */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
