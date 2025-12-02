f367ff1c0fc2e63c50b24334633b97bebf076ae7
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-12-02 13:31:17 +0100

    vec2.h: replace vec2_init() with struct initialization.

--- vec2.h
+++ vec2.h
@@ -149,70 +149,110 @@ extern "C" {
 
 #  endif /* __cplusplus */
 
-MATHAPI vec2 vec2_init(const float x, const float y) { return ((vec2) { .x = x, .y = y } ); }
+MATHAPI vec2 vec2_init(const float x, const float y) {
+    return ((vec2) { .x = x, .y = y } );
+}
 
-MATHAPI vec2 vec2_copy(const vec2 v0) { return ((vec2) { .x = v0.x, .y = v0.y } ); }
+MATHAPI vec2 vec2_copy(const vec2 v0) {
+    return ((vec2) { .x = v0.x, .y = v0.y } );
+}
 
-MATHAPI vec2 vec2_zero(void) { return (vec2_init(0.0, 0.0)); }
+MATHAPI vec2 vec2_zero(void) {
+    return ((vec2) { .x = 0.0, .y = 0.0 } );
+}
 
-MATHAPI vec2 vec2_one(void) { return (vec2_init(1.0, 1.0)); }
+MATHAPI vec2 vec2_one(void) {
+    return ((vec2) { .x = 1.0, .y = 1.0 } );
+}
 
-MATHAPI vec2 vec2_up(void) { return (vec2_init(0.0, 1.0)); }
+MATHAPI vec2 vec2_up(void) {
+    return ((vec2) { .x = 0.0, .y = 1.0 } );
+}
 
-MATHAPI vec2 vec2_down(void) { return (vec2_init(0.0, -1.0)); }
+MATHAPI vec2 vec2_down(void) {
+    return ((vec2) { .x = 0.0, .y = -1.0 } );
+}
 
-MATHAPI vec2 vec2_left(void) { return (vec2_init(-1.0, 0.0)); }
+MATHAPI vec2 vec2_left(void) {
+    return ((vec2) { .x = -1.0, .y = 0.0 } );
+}
 
-MATHAPI vec2 vec2_right(void) { return (vec2_init(1.0, 0.0)); }
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-MATHAPI vec2 vec2_add(const vec2 v0, const vec2 v1) { return (vec2_init(v0.x + v1.x, v0.y + v1.y)); }
-
-MATHAPI vec2 vec2_addf(const vec2 v0, const float f) { return (vec2_init(v0.x + f, v0.y + f)); }
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-MATHAPI vec2 vec2_sub(const vec2 v0, const vec2 v1) { return (vec2_init(v0.x - v1.x, v0.y - v1.y)); }
-
-MATHAPI vec2 vec2_subf(const vec2 v0, const float f) { return (vec2_init(v0.x - f, v0.y - f)); }
-
-/* ---------------------------------------------------------------------------------------------------- */
-
-MATHAPI vec2 vec2_mul(const vec2 v0, const vec2 v1)  { return (vec2_init(v0.x * v1.x, v0.y * v1.y)); }
-
-MATHAPI vec2 vec2_mulf(const vec2 v0, const float f) { return (vec2_init(v0.x * f, v0.y * f)); }
+MATHAPI vec2 vec2_right(void) {
+    return ((vec2) { .x = 1.0, .y = 0.0 } );
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
-MATHAPI vec2 vec2_div(const vec2 v0, const vec2 v1)  { return (vec2_init(v0.x / v1.x, v0.y / v1.y)); }
+MATHAPI vec2 vec2_add(const vec2 v0, const vec2 v1) {
+    return ((vec2) { .x = v0.x + v1.x, .y = v0.y + v1.y } );
+}
 
-MATHAPI vec2 vec2_divf(const vec2 v0, const float f) { return (vec2_init(v0.x / f, v0.y / f)); }
+MATHAPI vec2 vec2_addf(const vec2 v0, const float f) {
+    return ((vec2) { .x = v0.x + f, .y = v0.y + f } );
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
-MATHAPI bool vec2_equals(const vec2 v0, const vec2 v1) { return (v0.x == v1.x && v0.y == v1.y); }
+MATHAPI vec2 vec2_sub(const vec2 v0, const vec2 v1) {
+    return ((vec2) { .x = v0.x - v1.x, .y = v0.y - v1.y } );
+}
+
+MATHAPI vec2 vec2_subf(const vec2 v0, const float f) {
+    return ((vec2) { .x = v0.x - f, .y = v0.y - f } );
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
-MATHAPI vec2 vec2_pow(const vec2 v0, float f) { return (vec2_init(pow(v0.x, f), pow(v0.y, f))); }
+MATHAPI vec2 vec2_mul(const vec2 v0, const vec2 v1) {
+    return ((vec2) { .x = v0.x * v1.x, .y = v0.y * v1.y } );
+}
 
-MATHAPI vec2 vec2_pow2(const vec2 v0) { return (vec2_init(v0.x * v0.x, v0.y * v0.y)); }
+MATHAPI vec2 vec2_mulf(const vec2 v0, const float f) {
+    return ((vec2) { .x = v0.x * f, .y = v0.y * f } );
+}
 
-MATHAPI vec2 vec2_sqrt(const vec2 v0) { return (vec2_init(sqrt(v0.x), sqrt(v0.y))); }
+/* ---------------------------------------------------------------------------------------------------- */
+
+MATHAPI vec2 vec2_div(const vec2 v0, const vec2 v1) {
+    return ((vec2) { .x = v0.x / v1.x, .y = v0.y / v1.y } );
+}
+
+MATHAPI vec2 vec2_divf(const vec2 v0, const float f) {
+    return ((vec2) { .x = v0.x / f, .y = v0.y / f } );
+}
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+MATHAPI bool vec2_equals(const vec2 v0, const vec2 v1) {
+    return (v0.x == v1.x && v0.y == v1.y);
+}
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+MATHAPI vec2 vec2_pow(const vec2 v0, float f) {
+    return ((vec2) { .x = pow(v0.x, f), .y = pow(v0.y, f) } );
+}
+
+MATHAPI vec2 vec2_pow2(const vec2 v0) {
+    return ((vec2) { .x = v0.x * v0.x, .y = v0.y * v0.y });
+}
+
+MATHAPI vec2 vec2_sqrt(const vec2 v0) {
+    return ((vec2) { .x = sqrt(v0.x), .y = sqrt(v0.y) } );
+}
 
 /* ---------------------------------------------------------------------------------------------------- */
 
 MATHAPI vec2 vec2_clamp(const vec2 v0, const vec2 min, const vec2 max) {
-    return (vec2_init(clamp(v0.x, min.x, max.x), clamp(v0.y, min.y, max.y)));
+    return ((vec2) { .x = clamp(v0.x, min.x, max.x), .y = clamp(v0.y, min.y, max.y) } );
 }
 
 MATHAPI vec2 vec2_clampf(const vec2 v0, float min, float max) {
-    return (vec2_init(clamp(v0.x, min, max), clamp(v0.y, min, max)));
+    return ((vec2) { .x = clamp(v0.x, min, max), .y = clamp(v0.y, min, max) } );
 }
 
 MATHAPI vec2 vec2_clamp01(const vec2 v0) {
-    return (vec2_init(clamp01(v0.x), clamp01(v0.y)));
+    return ((vec2) { .x = clamp01(v0.x), .y = clamp01(v0.y) } );
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
@@ -238,8 +278,8 @@ MATHAPI vec2 vec2_normalize(const vec2 v0) {
     return (value);
 }
 
-MATHAPI vec2 vec2_scale(const vec2 v0, const float f0) {
-    return (vec2_mulf(v0, f0));
+MATHAPI vec2 vec2_scale(const vec2 v0, const float f) {
+    return (vec2_mulf(v0, f));
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
@@ -265,7 +305,7 @@ MATHAPI const char *vec2_string(const vec2 v0) {
 #  if defined (__cplusplus)
 #   if !defined (MATH_DISABLE_CPP_OPERATORS)
 
-MATHAPI vec2 operator + (const vec2 &v0, const vec2 &v1) { return (vec2_add(v0, v1)); }
+MATHAPI vec2 operator + (const vec2 &v0, const vec2 &v1)   { return (vec2_add(v0, v1)); }
 
 MATHAPI const vec2& operator += (vec2 &v0, const vec2 &v1) { return ((v0 = vec2_add(v0, v1))); }
 
@@ -281,9 +321,9 @@ MATHAPI vec2 operator / (const vec2 &v0, const vec2 &v1)   { return (vec2_div(v0
 
 MATHAPI const vec2& operator /= (vec2 &v0, const vec2 &v1) { return ((v0 = vec2_div(v0, v1))); }
 
-MATHAPI bool operator == (const vec2 &v0, const vec2 &v1) { return (vec2_equals(v0, v1)); }
+MATHAPI bool operator == (const vec2 &v0, const vec2 &v1)  { return (vec2_equals(v0, v1)); }
 
-MATHAPI bool operator != (const vec2 &v0, const vec2 &v1) { return (!vec2_equals(v0, v1)); }
+MATHAPI bool operator != (const vec2 &v0, const vec2 &v1)  { return (!vec2_equals(v0, v1)); }
 
 #   endif /* MATH_DISABLE_CPP_OPERATORS */
 #  endif /* __cplusplus */
